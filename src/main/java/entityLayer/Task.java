20e9ae81756ae6f42ee9290e58ab7846c9a3c4c6
MMachado <marcialmachado0522@gmail.com>
2022-11-04 18:19:08 -0400

    Added documentation for the Task interface.
    Methods and the class' creation info.

--- src/main/java/entityLayer/Task.java
+++ src/main/java/entityLayer/Task.java
@@ -1,14 +1,70 @@
 package entityLayer;
 
+/**
+ * A Task interface, implemented by the CommonTask class. Meant as a layer of abstraction.
+ * Created: 10/31/22
+ * Last updated: 11/4/2022
+ *
+ * @author MMachadoUofT
+ */
 public interface Task {
 
+    /**
+     * Get this Task's completion level.
+     *
+     * @return this Task's completion.
+     */
     int getCompletion();
+
+    /**
+     * Get this Task's name.
+     *
+     * @return this Task's name.
+     */
     String getName();
+
+    /**
+     * Get this Task's description.
+     *
+     * @return this Task's description.
+     */
     String getDescription();
+
+    /**
+     * Get this Task's ID attribute.
+     *
+     * @return this Task's unique ID.
+     */
     int getId();
 
+    /**
+     * Sets this Task's completion attribute to the passed number.
+     * If the attempted value is out of bounds (i.e. less than 0, greater than 100) return false. True otherwise.
+     *
+     * @param completion this task's new completion level.
+     * @return true if the completion level was valid, false otherwise.
+     */
     boolean setCompletion(int completion);
+
+    /**
+     * Sets this Task's name.
+     *
+     * @param name this Task's intended name.
+     */
     void setName(String name);
+
+    /**
+     * Sets this Task's description.
+     *
+     * @param description this Task's intended description.
+     */
     void setDescription(String description);
+
+    /**
+     * Returns whether this Task's completion is set to 100.
+     *
+     * @return true if this Task is complete, false otherwise.
+     */
+    boolean isComplete();
 
 }
