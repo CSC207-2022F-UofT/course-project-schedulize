79cc798bc69a464410d7f2e749ecd0bddb3f6670
MMachado <marcialmachado0522@gmail.com>
2022-11-10 13:22:28 -0500

    Added setup method to CommonTaskTests

--- src/test/java/entityLayer/CommonTaskTests.java
+++ src/test/java/entityLayer/CommonTaskTests.java
@@ -1,9 +1,21 @@
 package entityLayer;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CommonTaskTests {
+
+    Task t1;
+    Task t2;
+    Task t3;
+
+    @Before
+    public void setup() {
+        t1 = new CommonTask("n", "d");
+        t2 = new CommonTask("n", "d");
+        t3 = new CommonTask("n", "d");
+    }
 
     @Test
     public void testFirstTasksIdZeroThenOne() {
