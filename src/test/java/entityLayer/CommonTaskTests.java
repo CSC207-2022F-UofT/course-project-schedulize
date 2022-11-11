0228a7a0c3b1bffe403bd5b9d284fe7e81e6be2f
MMachado <marcialmachado0522@gmail.com>
2022-11-11 13:06:54 -0500

    Changes setup to BeforeEach
    For some reason, the setup method wasn't actually running; this seems to have fixed the issue.

--- src/test/java/entityLayer/CommonTaskTests.java
+++ src/test/java/entityLayer/CommonTaskTests.java
@@ -2,15 +2,16 @@ package entityLayer;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CommonTaskTests {
 
-    Task t1;
-    Task t2;
-    Task t3;
+    private Task t1;
+    private Task t2;
+    private Task t3;
 
-    @Before
+    @BeforeEach
     public void setup() {
         t1 = new CommonTask("n", "d");
         t2 = new CommonTask("n", "d");
@@ -19,20 +20,26 @@ public class CommonTaskTests {
 
     @Test
     public void testFirstTasksIdZeroThenOne() {
-        Task t = new CommonTask("Name", "Description");
-        Assertions.assertEquals(t.getId(), 0);
-        Task t2 = new CommonTask("Name", "Description");
+        Assertions.assertEquals(t1.getId(), 0);
         Assertions.assertEquals(t2.getId(), 1);
-        Assertions.assertNotEquals(t.getId(), t2.getId());
+        Assertions.assertNotEquals(t1.getId(), t2.getId());
     }
 
     @Test
     public void testCompletionOutOfBounds() {
-
+        t1.setCompletion(41);
+        t1.setCompletion(103);
+        Assertions.assertEquals(t1.getCompletion(), 41);
+        t2.setCompletion(-12);
+        Assertions.assertEquals(t2.getCompletion(), 0);
     }
 
-    @Test void testCompletionOutOfBoundsConstructor() {
-
+    @Test
+    void testCompletionOutOfBoundsConstructor() {
+        Task t = new CommonTask("n", "d", 132);
+        Task t_ = new CommonTask("n", "d", -12);
+        Assertions.assertEquals(t.getCompletion(), 100);
+        Assertions.assertEquals(t_.getCompletion(), 0);
     }
 
 }
