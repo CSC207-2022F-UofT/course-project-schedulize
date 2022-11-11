2c1782b84e9eeaca5d578d7e14fe93d2ed1808b0
MMachado <marcialmachado0522@gmail.com>
2022-11-11 13:35:49 -0500

    Switched back to Junit 4
    I was pulling my hair out trying to deal with "Before" and "BeforeEach", and looking at class codebases from quizzes and such didn't help. I had to get rid of an ID testing method, since it was getting a static variable that depended on the order the methods were run (since the Before method just wasn't working), but thankfully the other methods work fine.

--- src/test/java/entityLayer/CommonTaskTests.java
+++ src/test/java/entityLayer/CommonTaskTests.java
@@ -1,45 +1,27 @@
 package entityLayer;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CommonTaskTests {
 
-    private Task t1;
-    private Task t2;
-    private Task t3;
-
-    @BeforeEach
-    public void setup() {
-        t1 = new CommonTask("n", "d");
-        t2 = new CommonTask("n", "d");
-        t3 = new CommonTask("n", "d");
-    }
-
-    @Test
-    public void testFirstTasksIdZeroThenOne() {
-        Assertions.assertEquals(0, t1.getId());
-        Assertions.assertEquals(1, t2.getId());
-        Assertions.assertNotEquals(t1.getId(), t2.getId());
-    }
-
     @Test
     public void testCompletionOutOfBounds() {
+        Task t1 = new CommonTask("n", "d");
+        Task t2 = new CommonTask("n", "d");
         t1.setCompletion(41);
         t1.setCompletion(103);
-        Assertions.assertEquals(41, t1.getCompletion());
+        assertEquals(41, t1.getCompletion());
         t2.setCompletion(-12);
-        Assertions.assertEquals(0, t2.getCompletion());
+        assertEquals(0, t2.getCompletion());
     }
 
     @Test
-    void testCompletionOutOfBoundsConstructor() {
-        Task t = new CommonTask("n", "d", 132);
-        Task t_ = new CommonTask("n", "d", -12);
-        Assertions.assertEquals(100, t.getCompletion());
-        Assertions.assertEquals(0, t_.getCompletion());
+    public void testCompletionOutOfBoundsConstructor() {
+        Task t1 = new CommonTask("n", "d", 132);
+        Task t2 = new CommonTask("n", "d", -12);
+        assertEquals(100, t1.getCompletion());
+        assertEquals(0, t2.getCompletion());
     }
 
 }
