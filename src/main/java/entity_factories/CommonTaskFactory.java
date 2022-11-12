06a64393e531aa5cdaaac058e88c18f6fa354f00
MMachado <marcialmachado0522@gmail.com>
2022-11-11 20:43:40 -0500

    Created Task factory classes
    I'd forgotten to do this, but since these classes are rather simple, I'll include them in my implementation of the classes themselves. Then they can be used as soon as the actual entities are completed, as well.

---
+++ src/main/java/entity_factories/CommonTaskFactory.java
@@ -0,0 +1,11 @@
+package entity_factories;
+
+/**
+ * A CommonTaskFactory for creating CommonTask objects, implements the TaskFactory interface
+ * Created: 11/11/2022
+ * Last updated: 11/11/2022
+ *
+ * @author MMachadoUofT
+ */
+public class CommonTaskFactory implements TaskFactory {
+}
---
+++ src/main/java/entity_factories/TaskFactory.java
@@ -0,0 +1,11 @@
+package entity_factories;
+
+/**
+ * An interface for creating Tasks, implemented by the CommonTaskFactory class
+ * Created: 11/11/2022
+ * Last updated: 11/11/2022
+ *
+ * @author MMachadoUofT
+ */
+public interface TaskFactory {
+}
