425f0ba4e89b23e0663bfe996357af86bb9878c3
MMachado <marcialmachado0522@gmail.com>
2022-11-13 13:01:37 -0500

    Created create() methods in TaskFactory interface

--- src/main/java/entity_factories/TaskFactory.java
+++ src/main/java/entity_factories/TaskFactory.java
@@ -1,5 +1,7 @@
 package entity_factories;
 
+import entityLayer.Task;
+
 /**
  * An interface for creating Tasks, implemented by the CommonTaskFactory class
  * Created: 11/11/2022
@@ -8,4 +10,23 @@ package entity_factories;
  * @author MMachadoUofT
  */
 public interface TaskFactory {
+
+    /**
+     * Create a Task object with the given name and description
+     *
+     * @param name the Task's name
+     * @param description the Task's description
+     * @return the created Task
+     */
+    Task create(String name, String description);
+
+    /**
+     * Create a Task object with the given name, description, and duration
+     *
+     * @param name this Task's name
+     * @param description the Task's description
+     * @param duration the Task's duration in minutes
+     * @return the created Task
+     */
+    Task createWithDuration(String name, String description, int duration);
 }
