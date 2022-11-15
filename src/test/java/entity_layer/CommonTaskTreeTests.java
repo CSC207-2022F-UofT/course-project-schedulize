6226ef8899b77c53cea82df2e5aa8da12a126e44
MMachado <marcialmachado0522@gmail.com>
2022-11-15 13:36:16 -0500

    Created and implemented assorted TaskTree tests
    I'm making this commit primarily to change something else in the CommonTaskTree code, which, being unrelated to this testing code as of now, will be in a separate commit. So far, all tests have passed.

--- src/test/java/entity_layer/CommonTaskTreeTests.java
+++ src/test/java/entity_layer/CommonTaskTreeTests.java
@@ -1,6 +1,10 @@
 package entity_layer;
 
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -11,6 +15,39 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author MMachadoUofT
  */
 public class CommonTaskTreeTests {
+
+    /*
+    private TaskTree setupTree() {
+        TaskTree tree1 = new CommonTaskTree();
+        TaskTree tree2 = new CommonTaskTree();
+        TaskTree tree3 = new CommonTaskTree();
+        TaskTree tree4 = new CommonTaskTree();
+        TaskTree tree5 = new CommonTaskTree();
+        TaskTree tree6 = new CommonTaskTree();
+        TaskTree tree7 = new CommonTaskTree();
+
+        Task task1 = new CommonTask("n", "d");
+        Task task2 = new CommonTask("n", "d");
+        Task task3 = new CommonTask("n", "d");
+        Task task4 = new CommonTask("n", "d");
+        Task task5 = new CommonTask("n", "d");
+        Task task6 = new CommonTask("n", "d");
+        Task task7 = new CommonTask("n", "d");
+
+        tree1.setTask(task1);
+        tree2.setTask(task2);
+        tree3.setTask(task3);
+        tree4.setTask(task4);
+        tree5.setTask(task5);
+        tree6.setTask(task6);
+        tree7.setTask(task7);
+
+
+    }*/
+
+    private Task createTestTask() {
+        return new CommonTask("n", "d");
+    }
 
     @Test
     public void testAddSubtree() {
@@ -24,23 +61,42 @@ public class CommonTaskTreeTests {
     }
 
     @Test
-    public void testSubTreeHierarchyValidParents() {
-        assertEquals(4, 4);
-    }
-
-    @Test
     public void testNoSubtasksToList() {
-        // TODO: Write this test
+        TaskTree tree = new CommonTaskTree();
+        List<TaskTree> expected = new ArrayList<>();
+        expected.add(tree);
+        List<TaskTree> actual = tree.toList();
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testGetTaskTreeByIDThisTree() {
-        // TODO: Write this test
+        Task task = createTestTask();
+        int taskID = task.getId();
+        TaskTree tree = new CommonTaskTree();
+        tree.setTask(task);
+        assertEquals(task, tree.getChildTaskTreeByID(taskID).getTask());
     }
 
     @Test
     public void testGetTaskTreeByIDSubTree() {
-        // TODO: Write this test
+        TaskTree tree1 = new CommonTaskTree();
+        TaskTree tree2 = new CommonTaskTree();
+        TaskTree tree3 = new CommonTaskTree();
+
+        Task task1 = createTestTask();
+        Task task2 = createTestTask();
+        Task task3 = createTestTask();
+
+        tree1.addSubTaskTree(tree2);
+        tree1.addSubTaskTree(tree3);
+
+        tree1.setTask(task1);
+        tree2.setTask(task2);
+        tree3.setTask(task3);
+
+        assertEquals(task2, tree1.getChildTaskTreeByID(task2.getId()).getTask());
     }
 
     @Test
@@ -51,6 +107,41 @@ public class CommonTaskTreeTests {
     @Test
     public void testGetTaskTreeByIDNotFound() {
         // TODO: Write this test
+    }
+
+    /**
+     * Test that a Task(Tree) with two subTask(Trees) is 50% complete when one of its subTask(Tree)s is complete
+     */
+    @Test
+    public void testOneSubtaskComplete() {
+        TaskTree tree1 = new CommonTaskTree();
+        TaskTree tree2 = new CommonTaskTree();
+        TaskTree tree3 = new CommonTaskTree();
+
+        Task task1 = createTestTask();
+        Task task2 = createTestTask();
+        Task task3 = createTestTask();
+
+        tree1.addSubTaskTree(tree2);
+        tree1.addSubTaskTree(tree3);
+
+        tree1.setTask(task1);
+        tree2.setTask(task2);
+        tree3.setTask(task3);
+
+        tree2.completeTask();
+
+        assertEquals(50, tree1.getTask().getCompletion());
+    }
+
+    /**
+     * Test a tree with 3 subtrees, where each of those has 3 subtrees.
+     * One of the middle trees is completed.
+     * The expected outcome is that all of its subtrees are also completed, and its parent tree is 33% completed.
+     */
+    @Test
+    public void testCompleteTaskMiddleLayer() {
+
     }
 
 }
