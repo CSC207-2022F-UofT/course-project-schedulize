326c6b2b5e8fb1debfc989dc4417e3bb6dc5aaa1
David Adler <shankdaff@yahoo.com>
2022-11-15 01:29:12 -0500

    tests made compatible with new junit

--- src/test/java/UserStorageTests.java
+++ src/test/java/UserStorageTests.java
@@ -3,9 +3,9 @@ import config.Cryptograph;
 import entity_layer.CommonUser;
 import entity_layer.User;
 import config.UserStorage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 
 import java.io.IOException;
 import java.io.StreamCorruptedException;
@@ -15,24 +15,24 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class UserStorageTests {
-    UserStorage storage;
-    User user1;
-    String username1 = "deborah";
-    String password1 = "pokimane";
-    User user2;
-    String username2 = "loki";
-    String password2 = "novaScotia";
-    User user3;
-    String username3 = "nova";
-    String password3 = "dopeAwesome";
-    User user4;
-    String username4 = "maXX##";
-    String password4 = "criminology";
+    static UserStorage storage;
+    static User user1;
+    static String username1 = "deborah";
+    static String password1 = "pokimane";
+    static User user2;
+    static String username2 = "loki";
+    static String password2 = "novaScotia";
+    static User user3;
+    static String username3 = "nova";
+    static String password3 = "dopeAwesome";
+    static User user4;
+    static String username4 = "maXX##";
+    static String password4 = "criminology";
 
-    HashSet<String> usernames;
+    static HashSet<String> usernames;
 
-    @Before
-    public void setUp() throws IOException {
+    @BeforeAll
+    public static void setUp() throws IOException {
         Cryptograph cipher = new CommonCryptograph();
         storage = new UserStorage(cipher);
         user1 = new CommonUser(username1, "shale@lalal.com", password1);
@@ -43,32 +43,32 @@ public class UserStorageTests {
         usernames = new HashSet<>();
         String[] arr = {username1, username2, username3, username4};
         usernames.addAll(List.of(arr));
-        testAddingUsers();
-    }
-
-    @Test(timeout = 100)
-    public void testAddingUsers() throws IOException {
+        // add users
         storage.saveUser(user1);
         storage.saveUser(user2);
         storage.saveUser(user3);
         storage.saveUser(user4);
+    }
 
+    @Test
+    public void testAddingUsers() {
+        // test users properly added
         for (String username: usernames) {
             assert storage.usernameExists(username);
         }
     }
 
-    @Test(timeout = 100)
+    @Test
     public void testContainsUser() {
         assert storage.usernameExists(username1);
     }
 
-    @Test(timeout = 100)
+    @Test
     public void testNotContainsUser() {
         assert !storage.usernameExists("not a username");
     }
 
-    @Test(timeout = 100)
+    @Test
     public void testInitialization() {
         Cryptograph cipher = new CommonCryptograph();
         UserStorage ds = new UserStorage(cipher);
@@ -77,7 +77,7 @@ public class UserStorageTests {
         }
     }
 
-    @Test(timeout = 100)
+    @Test
     public void testUserLoad() throws IOException {
         User experimentUser = storage.loadUser(username1, password1);
         assertEquals(experimentUser.getUsername(), user1.getUsername());
@@ -85,7 +85,7 @@ public class UserStorageTests {
         assertEquals(experimentUser.getPassword(), user1.getPassword());
     }
 
-    @Test(timeout = 100)
+    @Test
     public void testInvalidUserLoad() throws IOException {
         try {
             storage.loadUser(username1, password2);
@@ -95,8 +95,8 @@ public class UserStorageTests {
         throw new RuntimeException();
     }
 
-    @After
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
         storage.removeUser(username1);
         storage.removeUser(username2);
         storage.removeUser(username3);
