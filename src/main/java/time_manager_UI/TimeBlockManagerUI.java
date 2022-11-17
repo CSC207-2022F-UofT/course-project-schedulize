033bb84c78805f1b2e5b1e83cf2c1f52a16e56b3
amira <amir.bare@mail.utoronto.ca>
2022-11-16 16:06:38 -0800

    Created TimeBlockManagerUI classes

---
+++ src/main/java/time_manager_UI/TimeBlockManagerUI.java
@@ -0,0 +1,13 @@
+package time_manager_UI;
+
+
+/**
+ * A TimeBlockManagerUi that uses JFrame to implement a time block window.
+ * Created:2022-11-14
+ * Last updated:2022-11-16
+ *
+ * @author Amir Bare
+ */
+public class TimeBlockManagerUI {
+
+}
