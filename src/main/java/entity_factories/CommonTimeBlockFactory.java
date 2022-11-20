fa8423ec276718aa1fd1ebfc54bdd864dbf854f6
MMachado <marcialmachado0522@gmail.com>
2022-11-20 18:58:38 -0500

    Created the TimeBlockFactory classes
    This includes one abstract, and one concrete.

---
+++ src/main/java/entity_factories/CommonTimeBlockFactory.java
@@ -0,0 +1,11 @@
+package entity_factories;
+
+/**
+ * A factory for creating CommonTimeBlocks, implements the TimeBlockFactory interface.
+ * Created: 11/20/2022
+ * Last updated: 11/20/2022
+ *
+ * @author MMachadoUofT
+ */
+public class CommonTimeBlockFactory implements TimeBlockFactory {
+}
---
+++ src/main/java/entity_factories/TimeBlockFactory.java
@@ -0,0 +1,11 @@
+package entity_factories;
+
+/**
+ * An abstract factory for creating TimeBlocks, implemented by the CommonTimeBlockFactory class
+ * Created: 11/20/2022
+ * Last updated: 11/20/2022
+ *
+ * @author MMachadoUofT
+ */
+public interface TimeBlockFactory {
+}
