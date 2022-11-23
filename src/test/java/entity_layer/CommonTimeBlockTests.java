7fc28e6273d37abf5f9056f82db7ebae08b95ea7
MMachado <marcialmachado0522@gmail.com>
2022-11-23 12:39:56 -0500

    Created methods for testing equals()
    This includes an implementation for the null comparison test.

--- src/test/java/entity_layer/CommonTimeBlockTests.java
+++ src/test/java/entity_layer/CommonTimeBlockTests.java
@@ -1,7 +1,36 @@
 package entity_layer;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CommonTimeBlockTests {
+
+    @Test
+    public void testEqualsNullComparison() {
+        LocalDateTime start = LocalDateTime.of(2022, Month.JULY, 14, 11, 30);
+        LocalDateTime end = LocalDateTime.of(2022, Month.JULY, 14, 14, 30);
+
+        TimeBlock t1 = new CommonTimeBlock(start, end);
+        TimeBlock t2 = null;
+
+        assertFalse(t1.equals(t2));
+    }
+
+    @Test
+    public void testEqualsReflexive() {
+
+    }
+
+    @Test
+    public void testEqualsSeparateObjectsTrue() {
+
+    }
+
+    @Test
+    public void testEqualsSeparateObjectsFalse() {
+
+    }
 }
