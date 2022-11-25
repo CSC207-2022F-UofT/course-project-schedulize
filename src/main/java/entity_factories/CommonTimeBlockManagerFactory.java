daae3da450f3b974dd74b8e8d90fdc62610ddf6b
MMachado <marcialmachado0522@gmail.com>
2022-11-25 17:52:53 -0500

    Implemented TimeBlockManagerFactory classes

--- src/main/java/entity_factories/CommonTimeBlockManagerFactory.java
+++ src/main/java/entity_factories/CommonTimeBlockManagerFactory.java
@@ -1,5 +1,12 @@
 package entity_factories;
 
+import entity_layer.CommonTimeBlockManager;
+import entity_layer.TimeBlock;
+import entity_layer.TimeBlockManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A class for creating CommonTimeBlockManagers.
  * Created: 11/25/2022
@@ -8,4 +15,25 @@ package entity_factories;
  * @author MMachadoUofT
  */
 public class CommonTimeBlockManagerFactory implements TimeBlockManagerFactory {
+
+    /**
+     * Returns a newly created object of type CommonTimeBlockManager with no TimeBlocks
+     *
+     * @return a TimeBlockManager
+     */
+    @Override
+    public TimeBlockManager createEmpty() {
+        return new CommonTimeBlockManager(new ArrayList<>(), new CommonTimeBlockFactory());
+    }
+
+    /**
+     * Returns a newly created object of type TimeBlockManager with the given TimeBlocks
+     *
+     * @param timeBlocks this TimeBlockManager's initial timeblocks
+     * @return a TimeBlockManager
+     */
+    @Override
+    public TimeBlockManager createWithTimeBlocks(List<TimeBlock> timeBlocks) {
+        return new CommonTimeBlockManager(timeBlocks, new CommonTimeBlockFactory());
+    }
 }
--- src/main/java/entity_factories/TimeBlockManagerFactory.java
+++ src/main/java/entity_factories/TimeBlockManagerFactory.java
@@ -1,5 +1,10 @@
 package entity_factories;
 
+import entity_layer.TimeBlock;
+import entity_layer.TimeBlockManager;
+
+import java.util.List;
+
 /**
  * An abstract factory for creating TimeBlockManagers.
  * Created: 11/25/2022
@@ -8,4 +13,19 @@ package entity_factories;
  * @author MMachadoUofT
  */
 public interface TimeBlockManagerFactory {
+
+    /**
+     * Returns a newly created object of type TimeBlockManager with no TimeBlocks
+     *
+     * @return a TimeBlockManager
+     */
+    TimeBlockManager createEmpty();
+
+    /**
+     * Returns a newly created object of type TimeBlockManager with the given TimeBlocks
+     *
+     * @param timeBlocks this TimeBlockManager's initial timeblocks
+     * @return a TimeBlockManager
+     */
+    TimeBlockManager createWithTimeBlocks(List<TimeBlock> timeBlocks);
 }
