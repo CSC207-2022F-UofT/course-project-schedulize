79b75c4091bbd43e7e8a4fbe14469559932b8cf9
David Adler <shankdaff@yahoo.com>
2022-11-26 02:42:12 -0500

    use case connected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,14 @@ import config.UserDataStoreGateway;
 import config.UserStorage;
 import entity_factories.CommonUserFactory;
 import entity_factories.UserFactory;
+import entity_layer.PasswordSuggester;
+import entity_layer.RandomPasswordGenerator;
 import use_cases.login.LoginController;
 import use_cases.login.LoginInputBoundary;
 import use_cases.login.LoginInteractor;
+import use_cases.suggest_password.PasswordSuggestionController;
+import use_cases.suggest_password.PasswordSuggestionInputBoundary;
+import use_cases.suggest_password.PasswordSuggestionInteractor;
 import use_cases.user_registration.UserRegistrationController;
 import use_cases.user_registration.UserRegistrationInteractor;
 
@@ -20,14 +25,18 @@ public class Main {
         Cryptograph cipher = new CommonCryptograph();
         UserFactory factory = new CommonUserFactory();
         UserDataStoreGateway storage = new UserStorage(cipher);
+        PasswordSuggester suggester = new RandomPasswordGenerator();
 
         UserRegistrationInteractor interactor = new UserRegistrationInteractor(factory, storage);
         UserRegistrationController registryController = new UserRegistrationController(interactor);
 
+        PasswordSuggestionInputBoundary suggestionInteractor = new PasswordSuggestionInteractor(suggester);
+        PasswordSuggestionController suggestionController = new PasswordSuggestionController(suggestionInteractor);
+
         LoginInputBoundary loginInteractor = new LoginInteractor(storage);
         LoginController loginController = new LoginController(loginInteractor);
 
-        CreateAccountUI createAccountWindow = new CreateAccountUI(windows, registryController);
+        CreateAccountUI createAccountWindow = new CreateAccountUI(windows, registryController, suggestionController);
         JFrame mainWindow = new LoginUI(windows, loginController);
     }
 }
