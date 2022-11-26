f869dac1d914726eab62291e31625dd5daa8181e
MMachado <marcialmachado0522@gmail.com>
2022-11-25 23:29:53 -0500

    Implemented Curriculum factories, created test suite
    This factory is a little more complicated, so I'll write some tests to make sure everything works as intended for my future use case.

--- src/main/java/entity_factories/CommonCurriculumFactory.java
+++ src/main/java/entity_factories/CommonCurriculumFactory.java
@@ -1,4 +1,43 @@
 package entity_factories;
 
+import entity_layer.CommonCurriculum;
+import entity_layer.Curriculum;
+import entity_layer.Task;
+import entity_layer.TaskTree;
+
+/**
+ * A concrete factory for creating CommonCurriculums
+ * Created: 11/25/2022
+ * Last updated: 11/25/2022
+ *
+ * @author MMachadoUofT
+ */
 public class CommonCurriculumFactory implements CurriculumFactory {
+
+    private final TaskTreeFactory taskTreeFactory;
+    private final TaskFactory taskFactory;
+    private final TimeBlockManagerFactory timeBlockManagerFactory;
+
+    public CommonCurriculumFactory(TaskTreeFactory taskTreeFactory, TaskFactory taskFactory,
+                                   TimeBlockManagerFactory timeBlockManagerFactory) {
+        this.taskTreeFactory = taskTreeFactory;
+        this.taskFactory = taskFactory;
+        this.timeBlockManagerFactory = timeBlockManagerFactory;
+    }
+
+    /**
+     * Returns a new CommonCurriculum object assigned the given name
+     *
+     * @param name this Curriculum's name
+     * @return the newly created Curriculum object
+     */
+    @Override
+    public Curriculum create(String name) {
+        TaskTree goal = taskTreeFactory.create();
+        Task goalTask = taskFactory.create(name, String.format("The %s curriculum", name));
+        goal.setTask(goalTask);
+
+        return new CommonCurriculum(goal, timeBlockManagerFactory.createEmpty(), timeBlockManagerFactory.createEmpty(),
+                name);
+    }
 }
--- src/main/java/entity_factories/CurriculumFactory.java
+++ src/main/java/entity_factories/CurriculumFactory.java
@@ -1,4 +1,21 @@
 package entity_factories;
 
+import entity_layer.Curriculum;
+
+/**
+ * An abstract factory for creating Curriculum objects
+ * Created: 11/25/2022
+ * Last updated: 11/25/2022
+ *
+ * @author MMachadoUofT
+ */
 public interface CurriculumFactory {
+
+    /**
+     * Returns a new Curriculum object assigned the given name
+     *
+     * @param name this Curriculum's name
+     * @return the newly created Curriculum object
+     */
+    Curriculum create(String name);
 }
