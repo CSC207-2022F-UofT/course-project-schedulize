27dbc3dc2a540c7d54ec1dfe9ea52c7c40dd90aa
MMachado <marcialmachado0522@gmail.com>
2022-11-26 11:12:58 -0500

    Implemented getCurriculum test for Schedule class

--- src/test/java/entity_layer/CommonScheduleTests.java
+++ src/test/java/entity_layer/CommonScheduleTests.java
@@ -1,5 +1,7 @@
 package entity_layer;
 
+import entity_factories.*;
+
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,4 +13,24 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author MMachadoUofT
  */
 public class CommonScheduleTests {
+
+    private final ScheduleFactory scheduleFactory = new CommonScheduleFactory(new CommonTimeBlockManagerFactory());
+    private final CurriculumFactory currFactory = new CommonCurriculumFactory(new CommonTaskTreeFactory(),
+                                                                              new CommonTaskFactory(),
+                                                                              new CommonTimeBlockManagerFactory());
+
+    @Test
+    public void testGetCurriculumByID() {
+        Schedule schedule = scheduleFactory.create();
+        Curriculum french = currFactory.create("French");
+        Curriculum java = currFactory.create("Java");
+        Curriculum spanish = currFactory.create("Spanish");
+        schedule.addCurriculum(french);
+        schedule.addCurriculum(java);
+        schedule.addCurriculum(spanish);
+        int expectedID = spanish.getID();
+
+        assertEquals(spanish, schedule.getCurriculum(expectedID));
+    }
+
 }
