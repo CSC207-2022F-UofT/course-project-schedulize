f0d25b480fd3e60e43e6b4db9621721d07b45ae1
MMachado <marcialmachado0522@gmail.com>
2022-11-26 10:50:50 -0500

    Created CommonScheduleTests class

---
+++ src/test/java/entity_layer/CommonScheduleTests.java
@@ -0,0 +1,14 @@
+package entity_layer;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * A test suite for the CommonSchedule class
+ * Created: 11/26/2022
+ * Last updated: 11/26/2022
+ *
+ * @author MMachadoUofT
+ */
+public class CommonScheduleTests {
+}
