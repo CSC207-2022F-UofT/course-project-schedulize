75095beac31e9112ebb8fa6ce3d0a9889b25faf6
Baylee <baylee.guiler@mail.utoronto.ca>
2022-11-28 11:10:37 -0500

    Set up Factories

--- src/test/java/complete_task/completeTaskUseCaseTest.java
+++ src/test/java/complete_task/completeTaskUseCaseTest.java
@@ -1,25 +1,49 @@
 package complete_task;
-import entity_layer.CommonUser;
-import entity_layer.Schedule;
-import entity_layer.User;
-import entity_layer.InMemoryUser;
-import entity_factories.CommonUserFactory;
+import entity_factories.*;
+import entity_layer.*;
+import use_cases.complete_task.*;
 
 import org.junit.Test;
 
 public class completeTaskUseCaseTest {
+    static CompleteTaskController controller;
+    // static CompleteTaskInputBoundary input;
+    static CompleteTaskUseCase interactor;
+    static CompleteTaskPresenter presenter;
+    static User activeUser;
 
     @Test
-    public void create(){
-        User activeUser = new CommonUserFactory().create
+    public void setup(){
+        TaskFactory taskFactory = new CommonTaskFactory();
+        // what do date times look like?
+        TimeBlock timeBlock = new CommonTimeBlockFactory().create();
+        TimeBlockManagerFactory timeBlockManager = new CommonTimeBlockManagerFactory().createWithTimeBlocks();
+        TaskTreeFactory taskTree = (TaskTreeFactory) new CommonTaskTreeFactory().create();
+        Curriculum curriculum = new CommonCurriculumFactory(taskTree, taskFactory, timeBlockManager).create("CSC207");
+        Schedule schedule = new CommonScheduleFactory(timeBlockManager).create();
+
+
+
+        activeUser = new CommonUserFactory().create
                 ("username", "email@email.com", "password");
+        Task readTextbook = new CommonTaskFactory().create("Read Textbook", "Read chapter 1 of Clean Architecture");
+        Task attendClass = new CommonTaskFactory().create("Attend Lecture", "Tuesday 6-8pm at Bahen Centre");
+        //add these tasks to the schedule and complete them
+        activeUser.setSchedule(schedule);
+        schedule.addCurriculum(curriculum);
+
+
+
+
+
         InMemoryUser.setActiveUser(activeUser);
-        // give the activeUser a schedule - no method yet
 
 
 
 
 
+        // input =
+        controller = new CompleteTaskController(input);
 
     }
 }
