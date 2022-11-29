73da508de9cc298cf0443528215aea12c1f0e743
Aayush Bhan <Bhanaayush@gmail.com>
2022-11-29 18:46:23 -0500

    created Interactor

--- src/main/java/use_cases/display_task_tree/DisplayTaskTreeInteractor.java
+++ src/main/java/use_cases/display_task_tree/DisplayTaskTreeInteractor.java
@@ -1,4 +1,22 @@
 package use_cases.display_task_tree;
 
-public class DisplayTaskTreeInteractor {
+/**
+ * A use case interactor for displaying TaskTree
+ * Created: 11/27/2022
+ * Last updated: 11/29/2022
+ *
+ * @author Aayush Bhan
+ */
+public class DisplayTaskTreeInteractor implements DisplayTaskTreeInputBoundary {
+    /**
+     * Executes the use case for creating a new Curriculum
+     *
+     * @param curriculumID the id of the Curriculum
+     */
+    @Override
+    public void displayTree(int curriculumID) {
+
+    }
+
+
 }
