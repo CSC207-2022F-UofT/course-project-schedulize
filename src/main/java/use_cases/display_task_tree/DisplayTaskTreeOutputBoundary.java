15af82b00c23599adbdd4ae61b87aa3e05ba3fc3
Aayush Bhan <Bhanaayush@gmail.com>
2022-11-29 18:45:56 -0500

    created Output Boundary

--- src/main/java/use_cases/display_task_tree/DisplayTaskTreeOutputBoundary.java
+++ src/main/java/use_cases/display_task_tree/DisplayTaskTreeOutputBoundary.java
@@ -1,4 +1,18 @@
 package use_cases.display_task_tree;
-
+/**
+ * An interface for the CreateCurriculumInteractor to pass information to the presenter
+ * Created: 11/27/2022
+ * Last updated 11/29/2022
+ *
+ * @author Aayush Bhan
+ */
 public interface DisplayTaskTreeOutputBoundary {
+
+    /**
+     * Prepares the displayed TaskTree for presentation
+     *
+     * @param tasktreedisplayModel the id of Curriculum's model
+     */
+    void displayTree(TaskTreeDisplayModel tasktreedisplayModel);
+
 }
