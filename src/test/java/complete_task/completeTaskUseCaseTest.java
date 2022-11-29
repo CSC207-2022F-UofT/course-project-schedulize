7ca178256fd34063ba4c03e0eb1c7bd5fedd0094
Baylee <baylee.guiler@mail.utoronto.ca>
2022-11-29 09:51:09 -0500

    Set up test method

--- src/test/java/complete_task/completeTaskUseCaseTest.java
+++ src/test/java/complete_task/completeTaskUseCaseTest.java
@@ -1,49 +1,48 @@
 package complete_task;
 import entity_factories.*;
 import entity_layer.*;
+import org.junit.Before;
 import use_cases.complete_task.*;
 
 import org.junit.Test;
 
 public class completeTaskUseCaseTest {
     static CompleteTaskController controller;
-    // static CompleteTaskInputBoundary input;
     static CompleteTaskUseCase interactor;
+    static CompletedTaskModel model;
     static CompleteTaskPresenter presenter;
     static User activeUser;
+    static Schedule schedule;
+    static Curriculum curriculum;
 
-    @Test
+    @Before
     public void setup(){
-        TaskFactory taskFactory = new CommonTaskFactory();
-        // what do date times look like?
-        TimeBlock timeBlock = new CommonTimeBlockFactory().create();
-        TimeBlockManagerFactory timeBlockManager = new CommonTimeBlockManagerFactory().createWithTimeBlocks();
-        TaskTreeFactory taskTree = (TaskTreeFactory) new CommonTaskTreeFactory().create();
-        Curriculum curriculum = new CommonCurriculumFactory(taskTree, taskFactory, timeBlockManager).create("CSC207");
-        Schedule schedule = new CommonScheduleFactory(timeBlockManager).create();
-
-
+        curriculum = new PrebuiltCurriculumFactory().create("CSC207");
+        schedule = new PrebuiltScheduleFactory().create();
 
         activeUser = new CommonUserFactory().create
                 ("username", "email@email.com", "password");
         Task readTextbook = new CommonTaskFactory().create("Read Textbook", "Read chapter 1 of Clean Architecture");
         Task attendClass = new CommonTaskFactory().create("Attend Lecture", "Tuesday 6-8pm at Bahen Centre");
-        //add these tasks to the schedule and complete them
+
         activeUser.setSchedule(schedule);
         schedule.addCurriculum(curriculum);
 
+        TaskTree readTextbookTT = new CommonTaskTreeFactory().createWithTask(readTextbook.getName(), readTextbook.getDescription());
+        TaskTree attendClassTT = new CommonTaskTreeFactory().createWithTask(attendClass.getName(), attendClass.getDescription());
+        curriculum.getGoal().addSubTaskTree(readTextbookTT);
+        curriculum.getGoal().addSubTaskTree(attendClassTT);
 
 
+        controller = new CompleteTaskController(interactor);
+        interactor = new CompleteTaskUseCase(presenter);
+        model = new CompletedTaskModel(curriculum.getName(), readTextbook.getName());
+        presenter = new CompleteTaskPresenter();
+    }
 
-
+    @Test
+    public void testCompleteTask(){
         InMemoryUser.setActiveUser(activeUser);
-
-
-
-
-
-        // input =
-        controller = new CompleteTaskController(input);
 
     }
 }
