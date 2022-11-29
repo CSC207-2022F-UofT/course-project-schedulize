a8dc296be1fa00137a05e302265036f8ac9565e3
Aayush Bhan <Bhanaayush@gmail.com>
2022-11-29 18:45:18 -0500

    created controller

--- src/main/java/use_cases/display_task_tree/DisplayTaskTreeController.java
+++ src/main/java/use_cases/display_task_tree/DisplayTaskTreeController.java
@@ -1,5 +1,20 @@
 package use_cases.display_task_tree;
-
+/**
+ * A controller for the creation of displaying a TaskTree use case
+ * Created: 11/27/2022
+ * Last updated: 11/29/2022
+ *
+ * @author Aayush Bhan
+ */
 public class DisplayTaskTreeController {
 
+
+        /**
+         * The controller method for displaying a tasktree belonging to the active user
+         *
+         * @param curriculumID the id of the Curriculum
+         */
+        public void displayTree(int curriculumID){
+
+        }
 }
