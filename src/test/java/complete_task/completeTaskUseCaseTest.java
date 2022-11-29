710eacc71b0cc2fcb5ffc8aae514c51ef7ff7a9d
Baylee <baylee.guiler@mail.utoronto.ca>
2022-11-29 10:15:38 -0500

    Add documentation

--- src/test/java/complete_task/completeTaskUseCaseTest.java
+++ src/test/java/complete_task/completeTaskUseCaseTest.java
@@ -1,9 +1,9 @@
 package complete_task;
 import entity_factories.*;
 import entity_layer.*;
-import use_cases.complete_task.*;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import use_cases.complete_task.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,11 +18,17 @@ public class completeTaskUseCaseTest {
     static Task readTextbook;
     static Task attendClass;
 
+    /**
+     * Set up the user's schedule with a curriculum called "CSC207" that has two
+     * tasks: "Read Textbook" and "Attend Lecture", and create instances of the CompleteTask
+     * controller, interactor and presenter for testing the complete task use case.
+     *
+     * @author Bmguiler
+     */
     @Before
     public void setup(){
         curriculum = new PrebuiltCurriculumFactory().create("CSC207");
         schedule = new PrebuiltScheduleFactory().create();
-
         activeUser = new CommonUserFactory().create
                 ("username", "email@email.com", "password");
         readTextbook = new CommonTaskFactory().create("Read Textbook", "Read chapter 1 of Clean Architecture");
@@ -36,12 +42,17 @@ public class completeTaskUseCaseTest {
         curriculum.getGoal().addSubTaskTree(readTextbookTT);
         curriculum.getGoal().addSubTaskTree(attendClassTT);
 
-
         controller = new CompleteTaskController(interactor);
         interactor = new CompleteTaskUseCase(presenter);
         presenter = new CompleteTaskPresenter();
     }
 
+    /**
+     * Test that the task "Read Textbook" was completed correctly and that the parent task's
+     * completion was updated accordingly.
+     *
+     * @author Bmguiler
+     */
     @Test
     public void testCompleteTask1(){
         InMemoryUser.setActiveUser(activeUser);
@@ -58,6 +69,12 @@ public class completeTaskUseCaseTest {
         assertEquals(attendClass.getCompletion(), 0);
     }
 
+    /**
+     * Test that the task "Attend Lecture" was completed correctly and that the parent task's
+     * completion was updated accordingly.
+     *
+     * @author Bmguiler
+     */
     @Test
     public void testCompleteTask2(){
         InMemoryUser.setActiveUser(activeUser);
