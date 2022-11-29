2d38764b2852c2d150f187e5bf5d91d1a727c38d
Aayush Bhan <Bhanaayush@gmail.com>
2022-11-29 18:52:43 -0500

    (TEMPORARY) Hardcoded TaskTree to UI for testing

--- src/main/java/task_tree_UI/TaskTreeUI.java
+++ src/main/java/task_tree_UI/TaskTreeUI.java
@@ -4,6 +4,7 @@ import UI.CentralWindow;
 import UI.TaskDependentWindow;
 import entity_factories.*;
 import entity_layer.*;
+import use_cases.display_task_tree.TaskTreeUIInterface;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.*;
@@ -22,7 +23,7 @@ import java.awt.event.*;
  */
 
 
-public class TaskTreeUI extends CentralWindow implements TaskDependentWindow {
+public class TaskTreeUI extends CentralWindow implements TaskDependentWindow, TaskTreeUIInterface {
     private JTree taskTree;
     private int displayId;
     private JPanel mainPanel;
@@ -167,54 +168,9 @@ public class TaskTreeUI extends CentralWindow implements TaskDependentWindow {
         ui.setWindowID(0);
         ui.setVisible(true);
 
-        // Add buttons
-
-        JButton button;
-
-         class MyFrame extends JFrame implements ActionListener {
-
-            JButton button;
-            JLabel label;
-
-            MyFrame() {
-
-                label = new JLabel();
-                label.setBounds(150, 250, 150, 150);
-                label.setVisible(false);
-
-                button = new JButton();
-                button.setBounds(100, 100, 250, 100);
-                button.addActionListener(this);
-                button.setText("add task");
-
-                button.setFocusable(false);
-                button.setHorizontalTextPosition(JButton.CENTER);
-                button.setVerticalTextPosition(JButton.BOTTOM);
-                button.setFont(new Font("Comic Sans", Font.BOLD, 25));
-                button.setIconTextGap(-15);
-                button.setForeground(Color.cyan);
-                button.setBackground(Color.lightGray);
-                button.setBorder(BorderFactory.createEtchedBorder());
-
-                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                this.setLayout(null);
-                this.setSize(500, 500);
-                this.setVisible(true);
-                this.add(button);
-                this.add(label);
-            }
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == button) {
-                    System.out.println("poo");
-                    button.setEnabled(false);
-                    label.setVisible(true);
-                }
-            }
         }
     }
-}
+
 
 
 
