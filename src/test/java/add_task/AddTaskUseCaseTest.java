bf5b49fa37a9af47aa3a88f1822fbc8609aadb9e
Baylee <baylee.guiler@mail.utoronto.ca>
2022-12-01 09:35:34 -0500

    tests written

--- src/test/java/add_task/AddTaskUseCaseTest.java
+++ src/test/java/add_task/AddTaskUseCaseTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import use_cases.add_task.*;
 import entity_layer.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class AddTaskUseCaseTest {
 
@@ -32,7 +34,57 @@ public class AddTaskUseCaseTest {
         controller = new AddTaskController(interactor);
     }
 
+    @Test
+    public void testAddTaskEmptyCurriculum(){
+        InMemoryUser.setActiveUser(activeUser);
+        Task parentTask = curriculum.getGoal().getTask();
+        int curriculumId = curriculum.getID();
+        int parentId = parentTask.getId();
+        AddTaskModel model = new AddTaskModel("Read Textbook",
+                "Read Chapter 1 from Clean Architecture",
+                curriculum.getName(), parentTask.getName());
 
+        controller.addTask("Read Textbook",
+                "Read Chapter 1 from Clean Architecture", parentId, curriculumId);
+        String expectedPresenter = "New task with name: Read Textbook and description: " +
+                "Read Chapter 1 from Clean Architecture was successfully added " +
+                "to the curriculum: CSC207 as a subtask of: CSC207.";
+        String actualPresenter = presenter.taskAdded(model);
+
+        assertEquals(expectedPresenter, actualPresenter);
+        assertTrue(curriculum.getGoal().hasSubTaskTrees());
+        assertSame("Read Textbook", curriculum.getGoal().getSubTaskTrees().get(0).getTask().getName());
+    }
+
+    @Test
+    public void testAddTaskNonEmptyCurriculum(){
+        InMemoryUser.setActiveUser(activeUser);
+
+        TaskTree readTextbookTT = new CommonTaskTreeFactory().createWithTask("Finish Coding", "Finish Use Cases for CSC207 Project");
+        Task finishCoding = readTextbookTT.getTask();
+        TaskTree attendClassTT = new CommonTaskTreeFactory().createWithTask("Attend Lecture", "Tuesday 6-8pm at Bahen Centre");
+        Task attendClass = attendClassTT.getTask();
+        curriculum.getGoal().addSubTaskTree(readTextbookTT);
+        curriculum.getGoal().addSubTaskTree(attendClassTT);
+
+        int parentId = finishCoding.getId();
+        int curriculumId = curriculum.getID();
+
+        AddTaskModel model = new AddTaskModel("Write Tests",
+                "Write tests for use cases", curriculum.getName(),
+                finishCoding.getName());
+
+        controller.addTask("Write Tests",
+                "Write tests for use cases", parentId, curriculumId);
+
+        String expectedPresenter = "New task with name: Write Tests  and description: " +
+                "Write tests for use cases was successfully added to the curriculum: CSC207" +
+                " as a subtask of: Finish Coding.";
+        String actualPresenter = presenter.taskAdded(model);
+
+        assertEquals(expectedPresenter, actualPresenter);
+
+    }
 
 
 
