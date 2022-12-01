dcfb93679acbee889c0e48c5699a9383d49e376b
Aayush Bhan <Bhanaayush@gmail.com>
2022-12-01 16:15:25 -0500

    Implemented TaskTreeTests

---
+++ src/test/java/task_tree_UI/TaskTreeTests.java
@@ -0,0 +1,18 @@
+package task_tree_UI;
+
+import org.junit.Test;
+public class TaskTreeTests {
+
+    private String name;
+    private int id;
+
+    @Test
+    public void TaskDisplayModel(String name, int id) {
+        this.name = name;
+        this.id = id;
+        {
+        }
+
+
+    }
+}
