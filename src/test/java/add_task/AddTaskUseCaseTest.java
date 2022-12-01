cf25fa134835aa407eac59cc7f3e9c468639859a
Baylee <baylee.guiler@mail.utoronto.ca>
2022-12-01 10:04:27 -0500

    finished writing tests

--- src/test/java/add_task/AddTaskUseCaseTest.java
+++ src/test/java/add_task/AddTaskUseCaseTest.java
@@ -40,12 +40,12 @@ public class AddTaskUseCaseTest {
         Task parentTask = curriculum.getGoal().getTask();
         int curriculumId = curriculum.getID();
         int parentId = parentTask.getId();
-        AddTaskModel model = new AddTaskModel("Read Textbook",
-                "Read Chapter 1 from Clean Architecture",
-                curriculum.getName(), parentTask.getName());
 
         controller.addTask("Read Textbook",
                 "Read Chapter 1 from Clean Architecture", parentId, curriculumId);
+        AddTaskModel model = new AddTaskModel("Read Textbook",
+                "Read Chapter 1 from Clean Architecture",
+                curriculum.getName(), parentTask.getName());
         String expectedPresenter = "New task with name: Read Textbook and description: " +
                 "Read Chapter 1 from Clean Architecture was successfully added " +
                 "to the curriculum: CSC207 as a subtask of: CSC207.";
@@ -57,25 +57,80 @@ public class AddTaskUseCaseTest {
     }
 
     @Test
+    public void testAddTwoTasks1(){
+        InMemoryUser.setActiveUser(activeUser);
+        Task parentTask1 = curriculum.getGoal().getTask();
+        int curriculumId = curriculum.getID();
+        int parentTask1Id = parentTask1.getId();
+        controller.addTask("Read Textbook",
+                "Read Chapter 1 from Clean Architecture", parentTask1Id, curriculumId);
+
+        Task parentTask2 = curriculum.getGoal().getSubTaskTrees().get(0).getTask();
+        int parentTask2Id = parentTask2.getId();
+        controller.addTask("Do Textbook Questions",
+                "Page 100 Questions 1-10", parentTask2Id, curriculumId);
+        AddTaskModel model = new AddTaskModel("Do Textbook Questions",
+                "Page 100 Questions 1-10", curriculum.getName(),
+                parentTask2.getName());
+
+        String expectedPresenter = "New task with name: Do Textbook Questions and description: " +
+                "Page 100 Questions 1-10 was successfully added to the curriculum: CSC207" +
+                " as a subtask of: Read Textbook.";
+        String actualPresenter = presenter.taskAdded(model);
+
+        assertEquals(expectedPresenter, actualPresenter);
+        assertTrue(curriculum.getGoal().hasSubTaskTrees());
+        assertSame(1, curriculum.getGoal().getSubTaskTrees().size());
+        assertTrue(curriculum.getGoal().getChildTaskTreeByID(parentTask2Id).hasSubTaskTrees());
+        assertSame("Do Textbook Questions", curriculum.getGoal().
+                getChildTaskTreeByID(parentTask2Id).getSubTaskTrees().get(0).getTask().getName());
+    }
+
+    @Test
+    public void testAddTwoTasks2(){
+        InMemoryUser.setActiveUser(activeUser);
+        Task parentTask = curriculum.getGoal().getTask();
+        int curriculumId = curriculum.getID();
+        int parentTaskId = parentTask.getId();
+        controller.addTask("Read Textbook",
+                "Read Chapter 1 from Clean Architecture", parentTaskId, curriculumId);
+        controller.addTask("Attend Lecture", "Tuesday 6-8pm at Bahen Centre",
+                parentTaskId, curriculumId);
+        AddTaskModel model = new AddTaskModel("Attend Lecture",
+                "Tuesday 6-8pm at Bahen Centre", curriculum.getName(),
+                parentTask.getName());
+
+        String expectedPresenter = "New task with name: Attend Lecture and description: " +
+                "Tuesday 6-8pm at Bahen Centre was successfully added to the curriculum: " +
+                "CSC207 as a subtask of: CSC207.";
+        String actualPresenter = presenter.taskAdded(model);
+
+        assertEquals(expectedPresenter, actualPresenter);
+        assertTrue(curriculum.getGoal().hasSubTaskTrees());
+        assertSame(2, curriculum.getGoal().getSubTaskTrees().size());
+        assertFalse(curriculum.getGoal().getSubTaskTrees().get(0).hasSubTaskTrees());
+        assertSame("Read Textbook", curriculum.getGoal().getSubTaskTrees().get(0).getTask().getName());
+        assertSame("Attend Lecture", curriculum.getGoal().getSubTaskTrees().get(1).getTask().getName());
+    }
+
+    @Test
     public void testAddTaskNonEmptyCurriculum(){
         InMemoryUser.setActiveUser(activeUser);
 
-        TaskTree readTextbookTT = new CommonTaskTreeFactory().createWithTask("Finish Coding", "Finish Use Cases for CSC207 Project");
-        Task finishCoding = readTextbookTT.getTask();
+        TaskTree finishCodingTT = new CommonTaskTreeFactory().createWithTask("Finish Coding", "Finish Use Cases for CSC207 Project");
+        Task finishCoding = finishCodingTT.getTask();
         TaskTree attendClassTT = new CommonTaskTreeFactory().createWithTask("Attend Lecture", "Tuesday 6-8pm at Bahen Centre");
-        Task attendClass = attendClassTT.getTask();
-        curriculum.getGoal().addSubTaskTree(readTextbookTT);
+        curriculum.getGoal().addSubTaskTree(finishCodingTT);
         curriculum.getGoal().addSubTaskTree(attendClassTT);
 
         int parentId = finishCoding.getId();
         int curriculumId = curriculum.getID();
 
+        controller.addTask("Write Tests",
+                "Write tests for use cases", parentId, curriculumId);
         AddTaskModel model = new AddTaskModel("Write Tests",
                 "Write tests for use cases", curriculum.getName(),
                 finishCoding.getName());
-
-        controller.addTask("Write Tests",
-                "Write tests for use cases", parentId, curriculumId);
 
         String expectedPresenter = "New task with name: Write Tests and description: " +
                 "Write tests for use cases was successfully added to the curriculum: CSC207" +
@@ -83,9 +138,10 @@ public class AddTaskUseCaseTest {
         String actualPresenter = presenter.taskAdded(model);
 
         assertEquals(expectedPresenter, actualPresenter);
-
+        assertTrue(curriculum.getGoal().hasSubTaskTrees());
+        assertSame(2, curriculum.getGoal().getSubTaskTrees().size());
+        assertTrue(curriculum.getGoal().getSubTaskTrees().get(0).hasSubTaskTrees());
+        assertSame("Write Tests", curriculum.getGoal().getSubTaskTrees().get(0).
+                getSubTaskTrees().get(0).getTask().getName());
     }
-
-
-
 }
