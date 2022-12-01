fcaf0f8ae50512545508fa3bf2ff0a30b6e46d23
Aayush Bhan <Bhanaayush@gmail.com>
2022-12-01 11:41:16 -0500

    Implemented TaskTree Interactor

--- src/main/java/use_cases/display_task_tree/DisplayTaskTreeInteractor.java
+++ src/main/java/use_cases/display_task_tree/DisplayTaskTreeInteractor.java
@@ -1,5 +1,12 @@
 package use_cases.display_task_tree;
 
+import entity_layer.InMemoryUser;
+import entity_layer.Task;
+import entity_layer.TaskTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A use case interactor for displaying TaskTree
  * Created: 11/27/2022
@@ -12,7 +19,7 @@ public class DisplayTaskTreeInteractor implements DisplayTaskTreeInputBoundary {
     private final DisplayTaskTreeOutputBoundary displayTaskTreeOutputBoundary;
 
     public DisplayTaskTreeInteractor(DisplayTaskTreeOutputBoundary displayTaskTreeOutputBoundary) {
-            this.displayTaskTreeOutputBoundary = displayTaskTreeOutputBoundary;
+        this.displayTaskTreeOutputBoundary = displayTaskTreeOutputBoundary;
     }
 
     /**
@@ -22,8 +29,36 @@ public class DisplayTaskTreeInteractor implements DisplayTaskTreeInputBoundary {
      */
     @Override
     public void displayTree(int curriculumID) {
-        //this.displayTaskTreeOutputBoundary.displayTree(curriculumID);
+        TaskTree goal = InMemoryUser.getActiveUser().getSchedule().getCurriculum(curriculumID).getGoal();
+        TaskTreeDisplayModel goalModel = createTaskTreeModel(goal);
+        displayTaskTreeOutputBoundary.displayTree(goalModel);
+    }
+
+    /**
+     * Creates a model TaskTree based on tree
+     *
+     * @param tree the original TaskTree entity
+     */
+    private static TaskTreeDisplayModel createTaskTreeModel(TaskTree tree) {
+        List<TaskTreeDisplayModel> subtrees = new ArrayList<>();
+        if (tree.hasSubTaskTrees()) {
+            for (TaskTree subtree : tree.getSubTaskTrees()) {
+                subtrees.add(createTaskTreeModel(subtree));
+            }
+        }
+        TaskDisplayModel taskModel = createTaskModel(tree.getTask());
+        return new TaskTreeDisplayModel(taskModel, subtrees);
+    }
+
+    /**
+     * Creates a model Task
+     *
+     * @param task the original Task entity
+     */
+    private static TaskDisplayModel createTaskModel(Task task) {
+        return new TaskDisplayModel(task.getName(), task.getId());
     }
 
 
 }
+
