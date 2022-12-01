84ff024f606f46e143031c9e74a0d511610701af
Aayush Bhan <Bhanaayush@gmail.com>
2022-12-01 16:09:46 -0500

    Implemented CurriculumDisplayModel

--- src/main/java/use_cases/display_task_tree/DisplayTaskTreePresenter.java
+++ src/main/java/use_cases/display_task_tree/DisplayTaskTreePresenter.java
@@ -2,16 +2,19 @@ package use_cases.display_task_tree;
 /**
  * A presenter for providing output data to the view through an interface.
  * Created: 11/27/2022
- * Last updated: 11/29/2022
+ * Last updated: 12/01/2022
  *
  * @author Aayush Bhan
  */
 public class DisplayTaskTreePresenter implements DisplayTaskTreeOutputBoundary {
 
-
+    private final TaskTreeUIInterface taskTreeUI;
+    public DisplayTaskTreePresenter (TaskTreeUIInterface taskTreeUI) {
+        this.taskTreeUI = taskTreeUI;
+    }
 
      @Override
-     public void displayTree(TaskTreeDisplayModel tasktreedisplayModel) {
-
+     public void displayTree(CurriculumDisplayModel curriculumDisplayModel) {
+        this.taskTreeUI.displayTree(curriculumDisplayModel);
     }
 }
