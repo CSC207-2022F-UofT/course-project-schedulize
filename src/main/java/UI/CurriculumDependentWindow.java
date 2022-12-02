34755be9d4b58c64362f92280674ffd66fa9ce49
David Adler <shankdaff@yahoo.com>
2022-12-01 21:39:58 -0500

    comments added

--- src/main/java/UI/CurriculumDependentWindow.java
+++ src/main/java/UI/CurriculumDependentWindow.java
@@ -1,5 +1,8 @@
 package UI;
 
+/**
+ * An interface for interacting with a window that has information dependent on a given curriculum object
+ */
 public interface CurriculumDependentWindow {
     void setCurriculumID(int curriculumId);
 }
--- src/main/java/UI/TaskDependentWindow.java
+++ src/main/java/UI/TaskDependentWindow.java
@@ -1,5 +1,8 @@
 package UI;
 
+/**
+ * An interface for interacting with a window that has information dependent on a given task object
+ */
 public interface TaskDependentWindow {
     void setTaskID(int taskID);
 }
