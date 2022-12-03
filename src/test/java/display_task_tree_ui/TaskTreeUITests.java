1ec0e2b0259d2d9cec3c9efae6abe5af0acdd905
Aayush Bhan <Bhanaayush@gmail.com>
2022-12-02 20:24:55 -0500

    incomplete tests

---
+++ src/test/java/display_task_tree_ui/TaskTreeUITests.java
@@ -0,0 +1,72 @@
+package display_task_tree_ui;
+
+import entity_factories.CommonTaskTreeFactory;
+import entity_factories.CommonUserFactory;
+import entity_factories.PrebuiltCurriculumFactory;
+import entity_factories.PrebuiltScheduleFactory;
+import entity_layer.*;
+import org.junit.jupiter.api.*;
+import task_display_ui.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTreeUITests {
+
+    static User activeUser;
+    static Schedule schedule;
+    static Curriculum curriculum;
+    static Task attendClass;
+
+
+    @BeforeAll
+    public static void setup(){
+        curriculum = new PrebuiltCurriculumFactory().create("CSC207");
+        schedule = new PrebuiltScheduleFactory().create();
+        activeUser = new CommonUserFactory().create
+                ("username", "email@email.com", "password");
+
+        activeUser.setSchedule(schedule);
+        schedule.addCurriculum(curriculum);
+
+        TaskTree attendClassTT = new CommonTaskTreeFactory().createWithTask("Attend Lecture",
+                "Tuesday 6-8pm at Bahen Centre");
+        attendClass = attendClassTT.getTask();
+        curriculum.getGoal().addSubTaskTree(attendClassTT);
+
+        InMemoryUser.setActiveUser(activeUser);
+    }
+
+    @Test
+    public void testPresenter(){
+        TaskUiTestView testView = new TaskUiTestView();
+        TaskUiOutputBoundary presenter = new TaskUiModelPresenter(testView);
+        TaskUiInputBoundary interactor = new TaskUiInteractor(presenter);
+        interactor.displayTask(curriculum.getID(), attendClass.getId());
+        TaskUiModel taskUiModel = testView.getTaskUiModel();
+        assertEquals(attendClass.getId(), taskUiModel.getTaskID());
+        assertEquals(curriculum.getID(), taskUiModel.getCurriculumID());
+        assertEquals(attendClass.getName(), taskUiModel.getName());
+        assertEquals(attendClass.getCompletion(), taskUiModel.getCompletion());
+    }
+
+    @Test
+    public void testInteractor(){
+        TaskUiTestPresenter presenter = new TaskUiTestPresenter();
+        TaskUiInputBoundary interactor = new TaskUiInteractor(presenter);
+        interactor.displayTask(curriculum.getID(), attendClass.getId());
+        TaskUiModel taskUiModel = presenter.getTaskUiModel();
+        assertEquals(attendClass.getId(), taskUiModel.getTaskID());
+        assertEquals(curriculum.getID(), taskUiModel.getCurriculumID());
+        assertEquals(attendClass.getName(), taskUiModel.getName());
+        assertEquals(attendClass.getCompletion(), taskUiModel.getCompletion());
+    }
+
+    @Test
+    public void testController(){
+        TaskUiTestInteractor interactor = new TaskUiTestInteractor();
+        TaskUiController controller = new TaskUiController(interactor);
+        controller.callInteractor(curriculum.getID(), attendClass.getId());
+        assertEquals(curriculum.getID(), interactor.getCurriculumID());
+    }
+
+}
---
+++ src/test/java/display_task_tree_ui/displayTaskTreeUseCaseTest.java
@@ -0,0 +1,13 @@
+package display_task_tree_ui;
+import entity_factories.*;
+import entity_layer.*;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import use_cases.complete_task.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class displayTaskTreeUseCaseTest {
+}
