b9bee27649728b853708a69068a4e7e6c7a6a32a
MMachado <marcialmachado0522@gmail.com>
2022-12-03 23:34:08 -0500

    Added Javadocs to Cryptograph interface

--- src/main/java/config/Cryptograph.java
+++ src/main/java/config/Cryptograph.java
@@ -13,6 +13,23 @@ import java.io.Serializable;
  * @author David Adler
  */
 public interface Cryptograph {
+
+    /**
+     * Encrypt and save object as byte stream
+     * @param object Serializable object to save
+     * @param oStream file to write object to
+     * @param password password for encryption
+     * @throws DataStorageMalfunction Saving and/or Encryption malfunction
+     */
     void encryptSave(Serializable object, OutputStream oStream, String password);
+
+    /**
+     * Decrypts bytes of file in input stream
+     * @param iStream file input stream
+     * @param password password for decryption
+     * @return decrypted object
+     * @throws IOException Decryption or Driver fail
+     * @throws DataStorageMalfunction Decryption or Cipher failure
+     */
     Object decryptLoad(InputStream iStream, String password) throws IOException;
 }
