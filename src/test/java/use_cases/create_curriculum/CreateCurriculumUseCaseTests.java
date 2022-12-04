a47abd69300fe1a980bd330cfcbd15b2699360ae
David Adler <shankdaff@yahoo.com>
2022-12-03 21:50:47 -0500

    comments added

--- src/test/java/use_cases/create_curriculum/CreateCurriculumUseCaseTests.java
+++ src/test/java/use_cases/create_curriculum/CreateCurriculumUseCaseTests.java
@@ -7,9 +7,19 @@ import entity_layer.InMemoryUser;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+/**
+ * A class that tests the creation use case for a new curriculum
+ * Created: 12/01/2022
+ * Last updated: 12/01/2022
+ *
+ * @author David Adler
+ */
 public class CreateCurriculumUseCaseTests {
     static CreateCurriculumController controller;
 
+    /**
+     * Sets up the create curriculum controller
+     */
     @BeforeAll
     public static void setUp() {
         CurriculumFactory factory = new PrebuiltCurriculumFactory();
@@ -17,6 +27,9 @@ public class CreateCurriculumUseCaseTests {
         controller = new CreateCurriculumController(interactor);
     }
 
+    /**
+     * Tests the creation of 3 curriculums for the InMemoryUser
+     */
     @Test
     public void testCurriculumCreation() {
         InMemoryUser.setActiveUser(new CommonUserFactory().create("username", "like@yaho.co",
