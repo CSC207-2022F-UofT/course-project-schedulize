cb775da26dd4dbcfa02ec23c92b57c199a8171bb
David Adler <shankdaff@yahoo.com>
2022-12-03 21:16:31 -0500

    display time block test cases written

--- src/test/java/use_cases/display_availability_time_blocks/DisplayAvailabilityTestCase.java
+++ src/test/java/use_cases/display_availability_time_blocks/DisplayAvailabilityTestCase.java
@@ -1,5 +1,6 @@
 package use_cases.display_availability_time_blocks;
 
+import entity_factories.CommonTimeBlockFactory;
 import entity_factories.CommonUserFactory;
 import entity_factories.PrebuiltScheduleFactory;
 import entity_layer.InMemoryUser;
@@ -9,18 +10,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import use_cases.display_availability_timeblocks.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * Class for testing the display availability use case
+ */
 public class DisplayAvailabilityTestCase {
     static DisplayAvailabilityTimeBlockController displayController;
     static DisplayAvailabilityTimeBlockInputBoundary displayInputBoundary;
     static DisplayAvailabilityTimeBlockOutputBoundary displayPresenter;
-    static DisplayAvailabilityTimeBlockInteractor displayInteractor;
     static User user;
     static Schedule schedule;
 
+    /**
+     * Sets up necessary entities
+     */
     @BeforeAll
     public static void setup(){
         user = new CommonUserFactory().create("Alex Scott", "scottieAlex@gmail.com", "ozzyozbourne");
@@ -29,16 +36,69 @@ public class DisplayAvailabilityTestCase {
         user.setSchedule(schedule);
         displayPresenter = new DisplayAvailabilityTimeBlockPresenter(new ArrayList<>());
         displayInputBoundary = new DisplayAvailabilityTimeBlockInteractor(displayPresenter);
-        displayController = new DisplayAvailabilityTimeBlockController(displayInteractor);
+        displayController = new DisplayAvailabilityTimeBlockController(displayInputBoundary);
     }
 
+    /**
+     * A test view to act as an observer for the output that the presenter returns
+     */
+    static class TestView implements AvailabilityUiInterface {
+
+        List<DisplayAvailabilityTimeBlockModel> models = new ArrayList<>();
+
+        @Override
+        public void drawAvailabilities(List<DisplayAvailabilityTimeBlockModel> availability) {
+            models = availability;
+        }
+
+        /**
+         * @return array of start times from the model
+         */
+        public String[] getStartTimes() {
+            String[] startTimes = new String[models.size()];
+            for (int i = 0; i < models.size(); i++) {
+                startTimes[i] = models.get(i).getStartTime().substring(0, 2);
+            }
+            return startTimes;
+        }
+
+        /**
+         * @return array of end times from the model
+         */
+        public String[] getEndTimes() {
+            String[] endTimes = new String[models.size()];
+            for (int i = 0; i < models.size(); i++) {
+                endTimes[i] = models.get(i).getEndTime().substring(0, 2);
+            }
+            return endTimes;
+        }
+    }
+
+    /**
+     * Tests that the return output from the presenter is the expected return output, based on the time right now
+     */
     @Test
     public void testDisplayUserAvailability(){
         InMemoryUser.setActiveUser(user);
-        List<DisplayAvailabilityTimeBlockModel> list = new ArrayList<>();
 
-        // TODO: FINISH WRITING TESTS
+        // create the test view
+        TestView view = new TestView();
+        displayPresenter.addAvailabilityObserver(view);
 
+        // add the 'now' time block to the user's availability
+        CommonTimeBlockFactory factory = new CommonTimeBlockFactory();
+        user.getSchedule().addAvailabilityBlock(factory.create(LocalDateTime.now(), LocalDateTime.now()));
+        displayController.displayAvailabilityTimeBlock();
+
+        // compare expected and actual start time
+        for (String startBlock: view.getStartTimes()) {
+            assert startBlock.equals(((Integer) (LocalDateTime.now().getHour())).toString());
+        }
+
+        // compare expected and actual end time
+        for (String startBlock: view.getEndTimes()) {
+            assert startBlock.equals(((Integer) (LocalDateTime.now().getHour())).toString());
+        }
     }
 
 }
